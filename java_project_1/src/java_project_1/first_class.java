28ac127a5ace98f0472a65a558e6dfcd73c84ff0
sheetal.jaswal <sheetal.jaswal@LP-5CD229DK30.HCLT.CORP.HCL.IN>
2023-03-07 15:48:11 +0530

    first commit

---
+++ java_project_1/src/java_project_1/first_class.java
@@ -0,0 +1,11 @@
+package java_project_1;
+
+public class first_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("abc");
+
+	}
+
+}
---
+++ java_project_1/src/java_project_1/second_class.java
@@ -0,0 +1,10 @@
+package java_project_1;
+
+public class second_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
